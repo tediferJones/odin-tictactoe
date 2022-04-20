cec221c17b036e41052c7dcaf5cf66b60662c931
ted <theo_drz@live.com>
2022-04-20 14:14:33 -0400

    added winner announcement and form for user to create players

--- script.js
+++ script.js
@@ -28,18 +28,30 @@ const PlayGame = (player1, player2) => {
     let currentBoard = Gameboard
     let currentPlayer = player1
     let winner = null
-    // let currentTurn = 0
+    let currentTurn = 0
 
     function runGame(location) {
-        if (winner === null) {
+        if (winner === null && currentTurn < 9) {
             currentBoard.placeMarker(currentPlayer, location)
             winCheck()
             switchTurns()
             display()
-        } 
-        if (winner != null) {
-            console.log(`WINNER IS ${winner.name}`)
-            // maybe append a div with little winner announcement here
+            currentTurn++
+            console.log(currentTurn)
+        }
+
+        if (winner != null || currentTurn === 9) {
+            announceWinner = document.createElement('div')
+            announceWinner.classList.add('announceWinner')
+            if (winner != null) {
+                // console.log("dooky")
+                announceWinner.innerHTML = `The winner is ${winner.name}`
+            } else if (currentTurn === 9) {
+                // console.log('ITS A TIE')
+                announceWinner.innerHTML = `It's a Tie!`
+            }
+            document.querySelector('#winMessage').innerHTML = ''
+            document.querySelector('#winMessage').appendChild(announceWinner)
         }
     }
 
@@ -82,12 +94,39 @@ const PlayGame = (player1, player2) => {
     }
 }
 
-p1 = createPlayer("jeff", "X")
-p2 = createPlayer("timmy", "O")
+const addPlayer = document.createElement('button')
+addPlayer.classList.add("submitBtn")
+addPlayer.innerHTML = 'Add Player'
+addPlayer.addEventListener('click', () => {
+    submitter()
+})
+document.querySelector('#formContainer').appendChild(addPlayer)
 
-let new_game = PlayGame(p1,p2)
-new_game.display()
+function submitter() {
+    let player1Name = document.querySelector('form').player1Name.value
+    let player2Name = document.querySelector('form').player2Name.value
+    let player1Marker = document.querySelector('form').player1Marker.value
 
-// win arrays can be stored in a "check win" function
-// check win function needs to reference the people playing the game
-// thus the function should live in the PlayGame object
+    let options = ['X', 'O']
+    options.splice(options.indexOf(player1Marker), 1)
+    // console.log(options)
+    let player2Marker = options[0]
+    // console.log(player1Name, player1Marker, player2Name, player2Marker)
+    PlayGame(createPlayer(player1Name, player1Marker), createPlayer(player2Name, player2Marker)).display()
+
+}
+
+// submitter()
+
+// p1 = createPlayer("jeff", "X")
+// p2 = createPlayer("timmy", "O")
+
+// let new_game = PlayGame(p1,p2)
+// new_game.display()
+
+// PlayGame(createPlayer("jeff", "X"), createPlayer('timmy', 'O')).display()
+
+// clean up UI
+// [ DONE ] make form for user to make their own player
+// make start/reset function (display function effectively starts the game, to reset, set board equal to default)
+// make computer function to play game
