32aa90169cac7764fdbe0a9c4d36be2560985800
ted <theo_drz@live.com>
2022-04-19 23:19:22 -0400

    added winCheck function and disabled markers from overwriting each other

--- script.js
+++ script.js
@@ -1,84 +1,93 @@
+
 const Gameboard = (() => {
     let board = [1,2,3,4,5,6,7,8,9]
 
-    function display() {
-        myContainer = document.querySelector('#container')
-        myContainer.innerHTML = ''        
-        for (i in board) {
-            // console.log(board[i])
-            let tempDiv = document.createElement('div')
-            tempDiv.classList.add('location')
-            tempDiv.innerHTML = board[i]
-            myContainer.appendChild(tempDiv)
-        }
-    }
-
     function placeMarker(player, location) {
-        return board.splice(location - 1, 1, player.marker)
+        // console.log(Number(location))
+        if (['1','2','3','4','5','6','7','8','9'].includes(location)) {
+            player.spotsTaken.push(board.splice(location - 1, 1, player.marker)[0])
+        }
     }
 
     return {
         board,
-        display,
-        placeMarker
+        placeMarker,
     }
 })();
 
 const createPlayer = (name, marker) => {
+    spotsTaken = []
     return {
         name,
-        marker
+        marker,
+        spotsTaken
     }
 }
 
 const PlayGame = (player1, player2) => {
-    function startGame() {
-        currentGame = Gameboard
-        currentTurn = 0
-        while (currentTurn < 9) {
-            if (currentTurn % 2 == 0) {
-                takeTurn(player1)
-            } else {
-                takeTurn(player2)
-            }
-            currentTurn++
-            console.log(`CURRENT TURN IS ${currentTurn}`)      
+    let currentBoard = Gameboard
+    let currentPlayer = player1
+    let winner = null
+    // let currentTurn = 0
+
+    function runGame(location) {
+        if (winner === null) {
+            currentBoard.placeMarker(currentPlayer, location)
+            winCheck()
+            switchTurns()
+            display()
+        } 
+        if (winner != null) {
+            console.log(`WINNER IS ${winner.name}`)
+            // maybe append a div with little winner announcement here
         }
     }
 
-    function takeTurn(player) {
-        console.log(player.marker)
+    function switchTurns() {
+        if (currentPlayer === player1) {
+            currentPlayer = player2
+        } else if (currentPlayer === player2) {
+            currentPlayer = player1
+        }
+    }
+
+    function winCheck() {
+        const winners = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
+
+        for (aWin in winners) {
+            let checker = (playerArray, winArray) => winArray.every(value => playerArray.includes(value))
+
+            if (checker(currentPlayer.spotsTaken, winners[aWin])) {
+                winner = currentPlayer
+            }
+        }
+    }
+
+    function display() {
+        myContainer = document.querySelector('#container')
+        myContainer.innerHTML = ''        
+        for (i in currentBoard.board) {
+            let tempDiv = document.createElement('div')
+            tempDiv.classList.add('location')
+            tempDiv.innerHTML = currentBoard.board[i]
+            tempDiv.addEventListener('click', () => {
+                runGame(tempDiv.innerHTML)
+            })
+            myContainer.appendChild(tempDiv)
+        }
     }
 
     return {
-        startGame,
-        takeTurn
+        display
     }
 }
-
-// someshit = document.querySelector('body')
-// console.log(someshit)
 
 p1 = createPlayer("jeff", "X")
 p2 = createPlayer("timmy", "O")
 
 let new_game = PlayGame(p1,p2)
-//new_game.startGame()
-
-let board = Gameboard
-board.display()
-// console.log(board.board)
-board.placeMarker(p1, 9)
-console.log(board.placeMarker(p2, 7))
-
-board.display()
-// console.log(board.board)
-console.log('###########################')
-// console.log(board.board)
-// board.display()
-// console.log(board.board)
-// board.display()
-// console.log(board.board)
+new_game.display()
 
 // win arrays can be stored in a "check win" function
-// 
+// check win function needs to reference the people playing the game
+// thus the function should live in the PlayGame object
