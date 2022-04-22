c2f6956ad2cc94b077027446674ef3b31ba271f7
ted <theo_drz@live.com>
2022-04-21 21:30:55 -0400

    Finished

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
 
 const Gameboard = (() => {
     let board = [1,2,3,4,5,6,7,8,9]
-
-    function placeMarker(player, location) {
-        // console.log(Number(location))
-        if (['1','2','3','4','5','6','7','8','9'].includes(location)) {
-            player.spotsTaken.push(board.splice(location - 1, 1, player.marker)[0])
-        }
-    }
-
-    return {
-        board,
-        placeMarker,
-    }
+    return { board }
 })();
 
 const createPlayer = (name, marker) => {
@@ -26,32 +15,47 @@ const createPlayer = (name, marker) => {
 
 const PlayGame = (player1, player2) => {
     let currentBoard = Gameboard
-    let currentPlayer = player1
+    let currentPlayer = [player1, player2][Math.floor(Math.random() * 2)]
     let winner = null
     let currentTurn = 0
+    addResetBtn()
+    display()
 
     function runGame(location) {
         if (winner === null && currentTurn < 9) {
-            currentBoard.placeMarker(currentPlayer, location)
+            placeMarker001(currentPlayer, location)
             winCheck()
             switchTurns()
             display()
             currentTurn++
-            console.log(currentTurn)
         }
 
         if (winner != null || currentTurn === 9) {
             announceWinner = document.createElement('div')
             announceWinner.classList.add('announceWinner')
             if (winner != null) {
-                // console.log("dooky")
                 announceWinner.innerHTML = `The winner is ${winner.name}`
             } else if (currentTurn === 9) {
-                // console.log('ITS A TIE')
                 announceWinner.innerHTML = `It's a Tie!`
             }
             document.querySelector('#winMessage').innerHTML = ''
             document.querySelector('#winMessage').appendChild(announceWinner)
+        }
+    }
+
+    function placeMarker001(player, location) {
+        if (['1','2','3','4','5','6','7','8','9'].includes(location)) {
+            player.spotsTaken.push(currentBoard.board.splice(location - 1, 1, player.marker)[0])
+        }
+    }
+
+    function winCheck() {
+        const winners = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
+        for (let aWin in winners) {
+            let checker = (playerArray, winArray) => winArray.every(value => playerArray.includes(value))
+            if (checker(currentPlayer.spotsTaken, winners[aWin])) {
+                winner = currentPlayer
+            }
         }
     }
 
@@ -60,18 +64,6 @@ const PlayGame = (player1, player2) => {
             currentPlayer = player2
         } else if (currentPlayer === player2) {
             currentPlayer = player1
-        }
-    }
-
-    function winCheck() {
-        const winners = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
-
-        for (aWin in winners) {
-            let checker = (playerArray, winArray) => winArray.every(value => playerArray.includes(value))
-
-            if (checker(currentPlayer.spotsTaken, winners[aWin])) {
-                winner = currentPlayer
-            }
         }
     }
 
@@ -87,46 +79,48 @@ const PlayGame = (player1, player2) => {
             })
             myContainer.appendChild(tempDiv)
         }
+        document.querySelector('#currentPlayer').innerHTML = `It's ${currentPlayer.name}'s turn`
     }
 
-    return {
-        display
+    function addResetBtn() {
+        const resetBtn = document.createElement('button')
+        resetBtn.classList.add('resetBtn')
+        resetBtn.innerHTML = 'RESET'
+        resetBtn.addEventListener('click', () => {
+            currentBoard.board = [1,2,3,4,5,6,7,8,9]
+            player1.spotsTaken = []
+            player2.spotsTaken = []
+            currentTurn = 0
+            winner = null
+            document.querySelector('#winMessage').innerHTML = ''
+            display()
+        })
+        document.querySelector('body').appendChild(resetBtn)
     }
 }
 
 const addPlayer = document.createElement('button')
 addPlayer.classList.add("submitBtn")
-addPlayer.innerHTML = 'Add Player'
+addPlayer.innerHTML = 'Start Game'
 addPlayer.addEventListener('click', () => {
-    submitter()
+    let player1Name = document.querySelector('form').player1Name.value
+    let player1Marker = document.querySelector('form').player1Marker.value
+
+    let player2Name = document.querySelector('form').player2Name.value
+    let options = ['X', 'O']
+    options.splice(options.indexOf(player1Marker), 1)
+    let player2Marker = options[0]
+
+    // console.log(document.querySelector('form').player1IsComputer.value)
+    
+    // adding hidden class to #container didnt work so, this is a work around
+    document.querySelector('#container').style.backgroundColor = 'black'
+    document.querySelector('#formContainer').style.display = 'none'
+    PlayGame(createPlayer(player1Name, player1Marker), createPlayer(player2Name, player2Marker))
 })
 document.querySelector('#formContainer').appendChild(addPlayer)
 
-function submitter() {
-    let player1Name = document.querySelector('form').player1Name.value
-    let player2Name = document.querySelector('form').player2Name.value
-    let player1Marker = document.querySelector('form').player1Marker.value
-
-    let options = ['X', 'O']
-    options.splice(options.indexOf(player1Marker), 1)
-    // console.log(options)
-    let player2Marker = options[0]
-    // console.log(player1Name, player1Marker, player2Name, player2Marker)
-    PlayGame(createPlayer(player1Name, player1Marker), createPlayer(player2Name, player2Marker)).display()
-
-}
-
-// submitter()
-
-// p1 = createPlayer("jeff", "X")
-// p2 = createPlayer("timmy", "O")
-
-// let new_game = PlayGame(p1,p2)
-// new_game.display()
-
-// PlayGame(createPlayer("jeff", "X"), createPlayer('timmy', 'O')).display()
-
 // clean up UI
-// [ DONE ] make form for user to make their own player
-// make start/reset function (display function effectively starts the game, to reset, set board equal to default)
+// [ DONE ] make form for user to make their own players
+// [ DONE ] make start/reset function (display function effectively starts the game, to reset, set board equal to default and reset player spotsTaken arr)
 // make computer function to play game
